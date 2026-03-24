03d00140ab3a656436252c3fcb83d3a4b12a58fb
RishiRao123 <rishiraopuskuri@gmail.com>
2026-03-24 16:41:19 +0530

    Added dto's

---
+++ delivery-service/src/main/java/org/raoamigos/deliveryservice/dto/DeliveryRequestDTO.java
@@ -0,0 +1,40 @@
+package org.raoamigos.deliveryservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class DeliveryRequestDTO {
+
+    @NotBlank(message = "Sender name is required")
+    private String senderName;
+
+    @NotNull(message = "Sender address is required")
+    private AddressDTO senderAddress;
+
+    @NotBlank(message = "Receiver name is required")
+    private String receiverName;
+
+    @NotNull(message = "Receiver address is required")
+    private AddressDTO receiverAddress;
+
+    @NotNull(message = "Package details are required")
+    private PackageDTO packageDetails;
+
+    @Data
+    public static class AddressDTO {
+        @NotBlank private String street;
+        @NotBlank
+        private String city;
+        @NotBlank private String state;
+        @NotBlank private String zipCode;
+    }
+
+    @Data
+    public static class PackageDTO {
+        private Double weight;
+        private Double dimensions;
+        @NotBlank private String description;
+    }
+}
