debff441c846eb28da5aae6a9df025c787a8ab7f
RishiRao123 <rishiraopuskuri@gmail.com>
2026-03-24 12:22:55 +0530

    created entity and repository

---
+++ auth-service/src/main/java/org/raoamigos/authservice/entity/Role.java
@@ -0,0 +1,7 @@
+package org.raoamigos.authservice.entity;
+
+public enum Role {
+
+    ROLE_ADMIN,
+    ROLE_CUSTOMER,
+}
---
+++ auth-service/src/main/java/org/raoamigos/authservice/entity/User.java
@@ -0,0 +1,37 @@
+package org.raoamigos.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
