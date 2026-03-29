5bf6685a2302bfd411d5d2373b83841cdd65b5d2
RishiRao123 <rishiraopuskuri@gmail.com>
2026-03-29 23:28:06 +0530

    Added new methods in delivery-service controller

--- admin-service/src/main/java/org/raoamigos/adminservice/dto/TrackingEventDTO.java
+++ admin-service/src/main/java/org/raoamigos/adminservice/dto/TrackingEventDTO.java
@@ -1,4 +1,22 @@
 package org.raoamigos.adminservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TrackingEventDTO {
+
+    private Long id;
+    private String trackingNumber;
+    private String status;
+    private String message;
+    private LocalDateTime timestamp;
+
 }
